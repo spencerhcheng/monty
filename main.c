b4c2c42dcf1683909ae79b867303b68e09cd22db
Stuart Kuredjian <108@holbertonschool.com>
2017-05-29 22:49:07 +0000

    remove putchar test

--- main.c
+++ main.c
@@ -17,8 +17,6 @@ int main(int argc, char **argv)
 	int exec_return = -1; /* TEST: was 0 */
 	cache_t *cache;
 
-	putchar('s');
-	
 	if (argc != 2)
 		raise_input_error();
 	cache = malloc(sizeof(cache_t));
