4bbcf32df4b5fb8179586861441f7aa591e3789f
Spencer <spencerhcheng@gmail.com>
2017-05-29 03:13:50 +0000

    Fixed error messages

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	/* check only one arg passed (other than ./monty */
 	if (argc != 2)
 	{
-		printf("Usage: %s file\n", argv[0]);
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	head = NULL;
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: can't open file %s\n", argv[1]);
+		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	line_number = 0;
